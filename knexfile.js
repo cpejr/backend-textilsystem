4c3f8e689e62b8a54d439683a655d6f7f6706fea
Paulo Estevao <paulo@PAULO.localdomain>
2021-01-14 19:07:35 -0300

    FIX setup back-end

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,33 @@
 // Update with your config settings.
-
+require("dotenv").config();
 module.exports = {
   development: {
-    client: "postgresql",
+    client: "pg",
     connection: {
-      filename: "./scr/database/db.pg",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
     },
-    migrations: {
-      directory: "./scr/database/migrations",
-    },
-    useNullAsDefault: true,
-  },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+    migrations: {
+      directory: "./src/database/migrations",
     },
     pool: {
       min: 2,
       max: 10,
     },
-    migrations: {
-      tableName: "knex_migrations",
-    },
   },
 
   production: {
-    client: "postgresql",
+    client: "pg",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
     },
     pool: {
       min: 2,
